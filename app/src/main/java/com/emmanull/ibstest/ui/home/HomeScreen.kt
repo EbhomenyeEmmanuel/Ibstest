60be1e4c3b53978ae8257b384c1d91742391aa04
Emmanuel.Ebhomenye <eebhomenye@yahoo.com>
2022-10-02 22:47:16 +0100

    Completed home screen design

--- app/src/main/java/com/emmanull/ibstest/ui/home/HomeScreen.kt
+++ app/src/main/java/com/emmanull/ibstest/ui/home/HomeScreen.kt
@@ -3,6 +3,8 @@ package com.emmanull.ibstest.ui.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,12 +17,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.emmanull.ibstest.R
-import com.emmanull.ibstest.ui.components.IbsButton
 import com.emmanull.ibstest.ui.components.IbsTextField
 
 @Composable
@@ -28,17 +29,19 @@ fun HomeScreenRoute(
     onNavigate: (String) -> Unit,
     homeViewModel: HomeViewModel = viewModel()
 ) {
-
-    HomeScreen()
+    val list = homeViewModel.homeList
+    HomeScreen(list) {
+        homeViewModel.onSearchHomeItems()
+    }
 
 }
 
 @Composable
-private fun HomeScreen() {
+private fun HomeScreen(list: List<HomeItem>, onSearch: (String) -> Unit) {
     val focusManager = LocalFocusManager.current
     var searchKey by rememberSaveable { mutableStateOf("") }
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .background(Color.White)
             .wrapContentSize()
@@ -48,23 +51,76 @@ private fun HomeScreen() {
         verticalArrangement = Arrangement.Center
     ) {
 
-        Text(
-            text = stringResource(id = R.string.search),
-            style = MaterialTheme.typography.h6,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            modifier = Modifier
-                .wrapContentSize()
-                .padding(bottom = 16.dp)
-        )
+        item {
+            Text(
+                text = stringResource(id = R.string.search),
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(bottom = 8.dp)
+            )
+        }
 
-        IbsTextField(
-            onTextChanged = { searchKey = it },
-            placeholder = { Text(text = stringResource(R.string.search_hint)) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 20.dp, bottom = 20.dp),
-        )
+        item {
+            IbsTextField(
+                onTextChanged = { searchKey = it },
+                placeholder = { Text(text = stringResource(R.string.search_hint)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 20.dp),
+                onKeyboardOptionsDone = {
+                    onSearch(searchKey)
+                }
+            )
+        }
 
+        item {
+            list.forEach {
+                HomeContentItem(homeItem = it)
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun HomeContentItem(homeItem: HomeItem) {
+    with(homeItem) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 8.dp)
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h6,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            Text(
+                text = "Account No. $accountNumber",
+                style = MaterialTheme.typography.body1,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            Text(
+                text = "Phone No. $phone",
+                style = MaterialTheme.typography.body1,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Divider(color = Color.Black, thickness = 1.dp)
+
+        }
     }
 }
--- app/src/main/java/com/emmanull/ibstest/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/emmanull/ibstest/ui/home/HomeViewModel.kt
@@ -2,5 +2,22 @@ package com.emmanull.ibstest.ui.home
 
 import androidx.lifecycle.ViewModel
 
+class HomeItem(
+    val id: Int,
+    val name: String,
+    val accountNumber: String,
+    val phone: String
+)
+
 class HomeViewModel : ViewModel() {
+    fun onSearchHomeItems() {
+
+    }
+
+    val homeList
+        get() = listOf<HomeItem>(
+            HomeItem(0, "Vitol Services Limited", "12345", "900009"),
+            HomeItem(1, "Vitol Services Limited2", "12345", "900009"),
+        )
+
 }
